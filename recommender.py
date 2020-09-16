b9a1b216474554a79607fbe732b3bb77952b44b9
ufukcbicici <ufukcbicici@yahoo.com>
2020-09-16 22:40:59 +0300

    Reaching the end

--- recommender.py
+++ recommender.py
@@ -15,6 +15,7 @@ from sklearn.decomposition import PCA
 import os
 import pickle
 from sklearn.metrics import mean_squared_error, r2_score
+from scipy.stats import t
 
 # from pandas.plotting import table
 from target_variable_scaler import HistogramScalerKBinsSupportingInf
@@ -191,8 +192,10 @@ class Recommender:
             score_money = self.scoreScalers["n13"].transform(y_money_pred)
             score = lambda_ * score_engagement + (1.0 - lambda_) * score_money
             df_x = pd.DataFrame(data=np.concatenate(
-                [score_engagement, score_money, score], axis=1),
-                                columns=["score_engagement_{0}".format(group),
+                [y_engagement_pred, y_money_pred, score_engagement, score_money, score], axis=1),
+                                columns=["y_engagement_pred_{0}".format(group),
+                                         "y_money_pred_{0}".format(group),
+                                         "score_engagement_{0}".format(group),
                                          "score_money_{0}".format(group),
                                          "score_{0}".format(group)])
             df_list.append(df_x)
@@ -205,3 +208,48 @@ class Recommender:
         result_path = csv_file[:-4] + "_recommendations.csv"
         final_df.to_csv(result_path, index=False)
         print("X")
+
+    # Calculate a global design recommendation
+    def determine_global_recommendation(self):
+        # Calculate the recommendations for every user in our original data
+        self.score_csv_file(csv_file="toydata_mltest.csv", lambda_=0.5)
+        # Open the recommendations file
+        df = pd.read_csv("toydata_mltest_recommendations.csv")
+        # For every player (row), get the predicted post-test engagement and monetization metrics for both groups.
+        post_test_engagements_A = df["y_engagement_pred_A"]
+        post_test_monetizations_A = df["y_money_pred_A"]
+        post_test_engagements_B = df["y_engagement_pred_B"]
+        post_test_monetizations_B = df["y_money_pred_B"]
+        # We are going to apply correlated t-tests to check whether there are statistically
+        # significant differences between the calculated post-test engagement and monetization means.
+        degree_of_freedoms = df.shape[0] - 1
+        # Test for significance in the engagement means between groups A and B
+        dif_engagement = post_test_engagements_A - post_test_engagements_B
+        # Is the difference in the means significantly different from zero?
+        M_engagement = dif_engagement.mean()
+        mu_engagement = 0
+        s_A = np.std(post_test_engagements_A, ddof=1)
+        s_B = np.std(post_test_engagements_B, ddof=1)
+        r_AB = np.corrcoef(post_test_engagements_A, post_test_engagements_B)[0, 1]
+        s_M = np.sqrt(((s_A**2.0 + s_B**2.0) - (2*r_AB*s_A*s_B)) / df.shape[0])
+        t_statistic = (np.abs(M_engagement) - mu_engagement) / s_M
+        two_tailed_probability = (1.0 - t.cdf(t_statistic, degree_of_freedoms)) * 2.0
+        # is_significant = two_tailed_probability >= 0.05
+        print("Post test engagement is {0} larger for group {1}. P value is {2}".format(
+            np.abs(M_engagement), "A" if M_engagement > 0 else "B", two_tailed_probability))
+
+        # Test for significance in the monetization means between groups A and B
+        dif_money = post_test_monetizations_A - post_test_monetizations_B
+        # Is the difference in the means significantly different from zero?
+        M_money = dif_money.mean()
+        mu_money = 0
+        s_A = np.std(post_test_monetizations_A, ddof=1)
+        s_B = np.std(post_test_monetizations_B, ddof=1)
+        r_AB = np.corrcoef(post_test_monetizations_A, post_test_monetizations_B)[0, 1]
+        s_M = np.sqrt(((s_A**2.0 + s_B**2.0) - (2*r_AB*s_A*s_B)) / df.shape[0])
+        t_statistic = (np.abs(M_money) - mu_money) / s_M
+        two_tailed_probability = (1.0 - t.cdf(t_statistic, degree_of_freedoms)) * 2.0
+        # is_significant = two_tailed_probability >= 0.05
+        print("Post test monetization is {0} larger for group {1}. P value is {2}".format(
+              np.abs(M_money), "A" if M_money > 0 else "B", two_tailed_probability))
+
